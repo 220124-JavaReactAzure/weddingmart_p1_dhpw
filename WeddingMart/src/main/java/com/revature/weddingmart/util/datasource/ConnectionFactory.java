25063a9ca06e879d8e1dedc9750da66daa759520
Philip Wentworth <guitartheist@gmail.com>
2022-02-14 14:40:33 -0500

    Created ConnectionFactory and added some skeletal code to ContextLoaderListener

--- WeddingMart/src/main/java/com/revature/weddingmart/util/datasource/ConnectionFactory.java
+++ WeddingMart/src/main/java/com/revature/weddingmart/util/datasource/ConnectionFactory.java
@@ -1,5 +1,49 @@
 package com.revature.weddingmart.util.datasource;
 
-public class ConnectionFactory {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class ConnectionFactory {
+	private static final ConnectionFactory connectionFactory = new ConnectionFactory();
+	private Properties prop = new Properties();
+	
+	static {
+		try {
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private ConnectionFactory() {
+		try {
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			prop.load(loader.getResourceAsStream("db.properties"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();  
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static ConnectionFactory getInstance() {
+		return connectionFactory;
+	}
+
+	public Connection getConnection() {
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(prop.getProperty("url"),prop.getProperty("admin"),prop.getProperty("password"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
 }
--- WeddingMart/src/main/java/com/revature/weddingmart/web/util/ContextLoaderListener.java
+++ WeddingMart/src/main/java/com/revature/weddingmart/web/util/ContextLoaderListener.java
@@ -1,5 +1,22 @@
 package com.revature.weddingmart.web.util;
 
-public class ContextLoaderListener {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
+public class ContextLoaderListener implements ServletContextListener {
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		//init DAOs
+		//init Services
+		//init Servlets
+		//add servlets
+		ServletContext context = sce.getServletContext();
+		//context.addServlet("ServetletName", initedServletFromAbove).addMapping("/url/*");
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		ServletContextListener.super.contextDestroyed(sce);
+	}
 }
