1f135e9192d29b7726a00ff5331a2d3353c637ff
Philip Wentworth <guitartheist@gmail.com>
2022-02-16 22:42:52 -0500

    UserService logging in place but still working on configuration

--- WeddingMart/src/main/java/com/revature/weddingmart/web/util/ContextLoaderListener.java
+++ WeddingMart/src/main/java/com/revature/weddingmart/web/util/ContextLoaderListener.java
@@ -1,9 +1,14 @@
 package com.revature.weddingmart.web.util;
 
+import org.apache.logging.log4j.Level;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.weddingmart.daos.users.UserDAO;
@@ -12,8 +17,12 @@ import com.revature.weddingmart.web.servlets.users.UserServlet;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
+	private final Logger logger = LogManager.getRootLogger();
+	
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
+		logger.info("contextInitialized");
+		
 		ObjectMapper mapper = new ObjectMapper();
 		
 		//init DAO
@@ -31,5 +40,6 @@ public class ContextLoaderListener implements ServletContextListener {
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		ServletContextListener.super.contextDestroyed(sce);
+		logger.info("contextDestroyed");
 	}
 }
